9ca138bf705bfe38c0c1de2fb7e2589926d0d872
Tobias Grosser <tobias@grosser.es>
2018-09-02 13:51:51 +0200

    isl_poly_is_[neg]infty: avoid implicit conversions
    Signed-off-by: Tobias Grosser <tobias@grosser.es>

--- isl_polynomial.c
+++ isl_polynomial.c
@@ -226,7 +226,9 @@ isl_bool isl_poly_is_infty(__isl_keep isl_poly *poly)
 	if (!cst)
 		return isl_bool_error;
 
-	return isl_int_is_pos(cst->n) && isl_int_is_zero(cst->d);
+	if (isl_int_is_pos(cst->n) && isl_int_is_zero(cst->d))
+		return isl_bool_true;
+	return isl_bool_false;
 }
 
 isl_bool isl_poly_is_neginfty(__isl_keep isl_poly *poly)
@@ -242,7 +244,9 @@ isl_bool isl_poly_is_neginfty(__isl_keep isl_poly *poly)
 	if (!cst)
 		return isl_bool_error;
 
-	return isl_int_is_neg(cst->n) && isl_int_is_zero(cst->d);
+	if (isl_int_is_neg(cst->n) && isl_int_is_zero(cst->d))
+		return isl_bool_true;
+	return isl_bool_false;
 }
 
 int isl_poly_is_one(__isl_keep isl_poly *poly)
